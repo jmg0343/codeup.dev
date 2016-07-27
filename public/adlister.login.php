e98d00c9c263dbed4907705eabee3182056faa79
Joey Garcia <jmg0343@gmail.com>
2016-07-27 14:59:56 -0500

    Fonts and Adlister Project

---
+++ public/adlister.login.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Heroic Features - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="heroic/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <!-- <link href="heroic/css/heroic-features.css" rel="stylesheet"> -->
+
+    <link rel="stylesheet" type="text/css" href="/css/adlister.login.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Adlister</a>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="#">Register</a>
+					</li>
+					<li>
+						<a href="#">Login</a>
+					</li>
+					<li>
+						<a href="#">Create AD</a>
+					</li>
+					<li>
+						<a href="#">Listings</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container -->
+	</nav>
+
+    <!-- Page Content -->
+    <div align="center" class="login">
+    	<form method="POST" action="/adlister.login.php">
+    		<p>
+    			<label for="username">Username</label>
+    			<input id="username" name="username" type="text">
+    		</p>
+    		<p>
+    			<label for="password">Password</label>
+    			<input id="password" name="password" type="password">
+    		</p>
+    		<p>
+    			<button id="submit-button" type="submit">Submit</button>
+    		</p>
+		</form>
+    </div>
+    <!-- /.container -->
+
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
