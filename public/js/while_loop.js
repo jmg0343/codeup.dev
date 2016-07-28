8c6d6aab6b4479d0af6809dd9f104d1766cabc17
Joey Garcia <jmg0343@gmail.com>
2016-07-28 16:05:48 -0500

    While Loop Exercise

---
+++ public/js/while_loop.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+// var cones = Math.floor(Math.random() * 5) + 1;
+// var conesLeft = (allCones - cones);
+
+
+
+
+//ALMOST WORKS. ELSE STATEMENT DOES NOT INITIALIZE
+
+
+console.log("I have " + allCones + " to sell.");
+do {
+	var cones = Math.floor(Math.random() * 5) + 1;
+	if (allCones >= cones) {
+		console.log(cones + " sold, " + (allCones - cones) + " left.");
+		allCones = allCones - cones;
+	}
+	else if (allCones < cones) {
+		console.log("I cannot sell you " + cones + " cones.");
+	} 
+	} while (allCones > 0);
+	console.log("Finally sold them all!");
