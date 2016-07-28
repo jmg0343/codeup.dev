dacd618b8b5ea1f21e02ba8b7a7941a2f4ca7d71
Joey Garcia <jmg0343@gmail.com>
2016-07-28 16:06:59 -0500

    Updated files

--- public/js/switch.js
+++ public/js/switch.js
@@ -1,33 +1,65 @@
 "use strict";
 
+// var luckyNumber = Math.floor(Math.random()* 6);
+
+// var number0 = 60;
+// var number1 = number0 - (60 * .1);
+// var number2 = number0 - (60 * .25);
+// var number3 = number0 - (60 * .35);
+// var number4 = number0 - (60 * .5);
+
+// switch (luckyNumber) {
+// 	case 0:
+// 		console.log(number0.toFixed(2) + " No discount");
+// 		break;
+// 	case 1:
+// 		console.log("lucky number: 1\n" + number1.toFixed(2) + "\n10% discount");
+// 		break;
+// 	case 2:
+// 		console.log("lucky number: 2\n" + number2.toFixed(2) + "\n25% discount");
+// 		break;
+// 	case 3:
+// 		console.log("lucky number: 3\n" + number3.toFixed(2) + "\n35% discount");
+// 		break;
+// 	case 4:
+// 		console.log("lucky numner: 4\n" + number4.toFixed(2) + "\n50% discount");
+// 		break;
+// 	default:
+// 		console.log("You get it for free!!")
+// 		break;
+// }
+
 var luckyNumber = Math.floor(Math.random()* 6);
 
-var number0 = 60;
-var number1 = number0 - (60 * .1);
-var number2 = number0 - (60 * .25);
-var number3 = number0 - (60 * .35);
-var number4 = number0 - (60 * .5);
+var priceBeforeDiscount = 60;
+var discountPercentage;
+var finalPrice;
 
 switch (luckyNumber) {
-	case 0:
-		console.log(number0.toFixed(2) + " No discount");
-		break;
 	case 1:
-		console.log(number1.toFixed(2) + ", 10% discount");
+		discountPercentage = .1;
 		break;
 	case 2:
-		console.log(number2.toFixed(2) + ", 25% discount");
+		discountPercentage = .25;
 		break;
 	case 3:
-		console.log(number3.toFixed(2) + ", 35% discount");
+		discountPercentage = .35;
 		break;
 	case 4:
-		console.log(number4.toFixed(2) + ", 50% discount");
+		discountPercentage = .5;
 		break;
-	default:
-		console.log("You get it for free!!")
+	case 5:
+		discountPercentage = 1;
 		break;
 }
+
+finalPrice = (1 - discountPercentage) * priceBeforeDiscount;
+
+console.log("Price before discount: $" + priceBeforeDiscount);
+console.log("luckyNumber: " + luckyNumber);
+console.log("Discount: " + (discountPercentage * 100) + "%");
+console.log("You will pay: $" + finalPrice.toFixed(2));
+
 
 
 var monthNumber = Math.floor((Math.random()* 12) +1);
