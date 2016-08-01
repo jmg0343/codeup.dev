153f4a4f50fd2e489f47643115dc7cbcaf505f77
Joey Garcia <jmg0343@gmail.com>
2016-08-01 14:18:28 -0500

    Function update to If/Else Exercise

--- public/js/if-else.js
+++ public/js/if-else.js
@@ -1,5 +1,17 @@
 "use strict";
 
+
+//  Exercise 1 ------------------------------------------------------------------------------
+
+function grades (grade1, grade2, grade3) {
+	var average = (grade1 + grade2 + grade3) / 3;
+	var finalAverage = (average > 80) ? "You're Awesome! " + average : "You need to practice more. " + average;
+	return finalAverage;
+}
+
+
+
+
 var grade_one = 70;
 
 var grade_two = 80;
@@ -13,6 +25,34 @@ if (average > 80) {
 } else {
 	console.log("You need to practice more.");
 }
+
+
+
+//Exercise 2 --------------------------------------------------------------------------------------
+
+function salesInfo(name, amountSpent) {
+	var customerName = name + " spent: $";
+	var total = (amountSpent > 200) ? amountSpent - (amountSpent * .35) : amountSpent;
+	var discountMessage = (amountSpent > 200) ? ". Discount was applied. Final payment: $" + total : " No discount applied. Final payment: $" + total;
+	return customerName + amountSpent + discountMessage;
+}
+
+
+
+// Code below prints out message with name
+// function customer (name) {
+// 	var customerName = name + " spent: ";
+// 	return customerName;
+// }
+
+// // Code below gives total price based on amount spent
+// function discountPrice (amountSpent) {
+// 	var total = (amountSpent > 200) ? amountSpent - (amountSpent * .35) : amountSpent;
+// 	return total;
+// }
+
+
+
 
 
 // discount on more than 200
@@ -44,6 +84,17 @@ if (georgeSpent <= applyDiscount) {
 } else {
 	console.log("George spent" + " $" + georgeSpent + ", discount was applied. Final payment: " + "$" + (georgeSpent - (georgeSpent * discount)));
 }
+
+
+
+// Exercise 3 -----------------------------------------------------------------------
+
+function decision () {
+ 	var flipACoin = Math.floor(Math.random()* 2);
+ 	var choice = (flipACoin == 0) ? "Buy a house" : "Buy a car";
+ 	return choice;
+}
+
 
 
 var flipACoin = Math.floor(Math.random()* 2);
