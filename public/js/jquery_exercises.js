98532a97718bb1af46cb1b286e1ea7ec17e159d1
Joey Garcia <jmg0343@gmail.com>
2016-08-11 11:28:46 -0500

    jQuery exercises

---
+++ public/js/jquery_exercises.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(document).ready(function () {
+	$(".list").css("font-size", "20px");
+	$(".yellowBackground").css("background-color", "yellow");
+	$("h1, li, p").click(function(){
+		alert("this is my heading");
+	});
+
+})
+
+	// $(".heading").click(function() {
+	// 	alert("click");
+	// });
