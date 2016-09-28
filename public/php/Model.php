d738b7120a8d9f82dd1ec5fefa163cb1d46970b1
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-09-28 16:53:49 -0500

    Magic Methods

---
+++ public/php/Model.php
@@ -0,0 +1,22 @@
+<?php
+
+class Model {
+	private $attributes = [];
+
+	public function __set($name, $value) {
+		$this->attributes[$name] = $value;
+	}
+
+	public function __get($name) {
+		if (array_key_exists($name, $this->attributes)) {
+			return $this->attributes[$name];
+		}
+		return null;
+	}
+
+}
+
+
+$attribute = new Model();
+$attribute->name = "Joey";
+echo $attribute->name;
