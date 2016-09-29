6b2bf50b0fdd0774bbb4ff0ce8759ea776c2c80f
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-09-29 10:57:41 -0500

    Updated exercise using static binding

--- public/php/Model.php
+++ public/php/Model.php
@@ -1,7 +1,10 @@
 <?php
 
+require_once "User.php";
+
 class Model {
 	private $attributes = [];
+	protected static $table;
 
 	public function __set($name, $value) {
 		$this->attributes[$name] = $value;
@@ -14,9 +17,15 @@ class Model {
 		return null;
 	}
 
+	public static function getTableName() {
+		return static::$table;
+	}
+
 }
 
 
-$attribute = new Model();
-$attribute->anyProperty=['joey', 'is', 'awesome'];
-var_dump($attribute->anyProperty);
+// $attribute = new Model();
+// $attribute->anyProperty=['joey', 'is', 'awesome'];
+// var_dump($attribute->anyProperty);
+
+echo User::getTableName() . PHP_EOL;
